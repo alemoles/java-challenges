627b7ff02c5d90f86fb4d13b5a7c396412132e7c
alemoles <alemolerio@gmail.com>
2022-12-31 14:02:04 -0300

    JC Function Challenge

---
+++ src/main/java/com/devstromo/JC_FunctionCalculation/FunctionChallenge.java
@@ -0,0 +1,17 @@
+package com.devstromo.JC_FunctionCalculation;
+
+import java.util.function.Function;
+
+public class FunctionChallenge {
+
+    public static void main(String... doYourBest) {
+        Function<Integer, Integer> add = x -> x + 2;
+
+        Function<Integer, Integer> sub = x -> x - 2;
+        Function<Integer, Integer> div = x -> x / 2;
+
+        Function<Integer, Integer> func = add.andThen(sub)
+            .andThen(div);
+        System.out.println(func.apply(2));
+    }
+}
