febf2fc5fb5d13661b2b6e9f66d768f2b5763985
alemoles <alemolerio@gmail.com>
2023-01-05 20:28:48 -0300

    JC List removeIf Wrappers Java Challenge

---
+++ src/main/java/com/devstromo/JC_13/ListRemoveIfChallenge.java
@@ -0,0 +1,24 @@
+package com.devstromo.JC_13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListRemoveIfChallenge {
+
+    public static void main(String... doYourBest) {
+        List<Integer> list = new ArrayList<>();
+        list.add(22);
+        list.add(12);
+        list.add(14);
+        list.add(128);
+
+        Integer prestoAge = 14;
+        Integer dungeonMasterAge = 128;
+        list.removeIf(e -> e == new Integer(22)
+          || e == dungeonMasterAge
+          || e == prestoAge
+          || e.equals(new Integer(22)));
+        System.out.println(list);
+
+    }
+}
